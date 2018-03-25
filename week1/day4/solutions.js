2a987f24d4541cf3202643ce61c9bde4eadf9556
jeremysaysossu <jeremysaysossu@gmail.com>
2018-03-25 14:55:44 -0500

    HTML Page

--- week1/day4/solutions.js
+++ week1/day4/solutions.js
@@ -1,15 +1,1 @@
-// A parameter is a variable in a method definition. When a method is called, the arguments are the data you pass into the method
 //
-// "Commit 1 - Verbal questions".
-
-// function pythagorean(sideA, sideB){
-//   return Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
-// }
-//
-//  console.log(pythagorean(4, 3));
-
-//  console shows an answer of 5
-//
-// wanted to study this and see if i am able to understand it.
-//
-// "Commit 4 - Pythagoras".
