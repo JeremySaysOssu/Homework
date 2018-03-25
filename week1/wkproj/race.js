3e96ca3850b7328104897758de2dfe1f0b8413ea
jeremysaysossu <jeremysaysossu@gmail.com>
2018-03-25 17:33:15 -0500

    update 530

--- week1/wkproj/race.js
+++ week1/wkproj/race.js
@@ -1,26 +1,7 @@
- display game with two color racing blocks with a start line and finish line with checker flag. black background
-
-have start button for user to start game or let buttons start game.
-
-two keyboard buttons will make players color blocks move.
-
-have aleart show winner 1 or 2
-
-function moveR() {
-
-}
-
-var redB;
-var blueB
-
-function startGame() {
-
-}
-
-function mvP1() {
-
-}
-
-function mvP2() {
-
-}
+//  display game with two color racing blocks with a start line and finish line with checker flag. black background
+//
+// have start button for user to start game or let buttons start game.
+//
+// two keyboard buttons will make players color blocks move.
+//
+// have aleart show winner 1 or 2
