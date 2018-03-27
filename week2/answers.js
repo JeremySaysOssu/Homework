2d246ac9b43b510b5f4e32eda07c40c275d52432
jeremysaysossu <jeremysaysossu@gmail.com>
2018-03-26 23:59:42 -0500

    accessed limbo

--- week2/answers.js
+++ week2/answers.js
@@ -1,21 +1,70 @@
-// A light switch that can be either on or off.
-//  a: arrays
-// A user's email address.
-//  a: object
-// A spaceship with a hull, laser blasters, tractor beam, and warp drive.
-//  a: string
-// A list of student names from our class.
+// // A light switch that can be either on or off.
+// //  a: arrays
+// // A user's email address.
+// //  a: object
+// // A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+// //  a: string
+// // A list of student names from our class.
+//
+// // A list of student names from our class, each with a location.
+// //  a: string and/or numbers
+// // A list of student names from our class, each with a location and each with a list of favorite tv shows.
+// // -----------------------------------
+//
+// var rainbow;
+//
+// rainbow = ["blue", "red", "green", "yellow", "orange", "indigo", "violet"];
+//
+// var first = rainbow[0];
+// var last = rainbow[rainbow.length - 6];
+//
+// console.log(rainbow.[0]);
+// -----------------------------
 
-// A list of student names from our class, each with a location.
-//  a: string and/or numbers
-// A list of student names from our class, each with a location and each with a list of favorite tv shows.
-// -----------------------------------
+// const crazyObject = {
+//   taco: [{meat: 'steak',
+//          cheese: ['panela', 'queso', 'chihuahua']},
+//          {meat: 'chicken',
+//           salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+//         ],
+//   larry: {
+//     nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+//     quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+//     characters: [{
+//       name: "Jeff",
+//       occupation: "manager"
+//     },
+//     {
+//       name: "funkhauser",
+//       occupation: "tv dude"
+//     },
+//     {
+//       name: "susie",
+//       occupation: "jeffs wife",
+//       favourtieHobby: "Swearing at Larry and Jeff"
+//     },
+//     ]
+//   }
+// }
+//
+// console.log(crazyObject.salsa, [5]);
 
-var rainbow;
+//  was having a hard tine with this one
 
-rainbow = ["blue", "red", "green", "yellow", "orange", "indigo", "violet"];
-
-var first = rainbow[0];
-var last = rainbow[rainbow.length - 6];
-
-console.log(rainbow.[0]);
+const inception = {
+   reality: {
+       dreamLayer1: {
+           dreamLayer2: {
+               dreamLayer3: {
+                   dreamLayer4: {
+                       dreamLayer5: {
+                           dreamLayer6: {
+                               limbo: "Joseph Gordon Levitt"
+                          }
+                       }
+                   }
+               }
+           }
+       }
+   }
+}
