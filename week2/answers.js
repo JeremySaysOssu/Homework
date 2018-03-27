e787e69166326847ad553f80c93bae5d9102a803
jeremysaysossu <jeremysaysossu@gmail.com>
2018-03-26 23:34:21 -0500

    created data struct

---
+++ week2/answers.js
@@ -0,0 +1,21 @@
+// A light switch that can be either on or off.
+//  a: arrays
+// A user's email address.
+//  a: object
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+//  a: string
+// A list of student names from our class.
+
+// A list of student names from our class, each with a location.
+//  a: string and/or numbers
+// A list of student names from our class, each with a location and each with a list of favorite tv shows.
+// -----------------------------------
+
+var rainbow;
+
+rainbow = ["blue", "red", "green", "yellow", "orange", "indigo", "violet"];
+
+var first = rainbow[0];
+var last = rainbow[rainbow.length - 6];
+
+console.log(rainbow.[0]);
